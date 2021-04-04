2eba3e6108ac3038cb0c9c477964a1101279e655
Rex-fortune <akpoyibofortune@gmail.com>
2021-04-03 19:35:54 -0700

    installing and setting up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import logo from "./Images/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img
+          src={logo}
+          alt="logo"
+          width="100%"
+          height="100%"
+          className="rounded App-logo"
+        />
+        Overall re-design awaiting a new app launch in progress
+        <h1>Available shortly</h1>
+      </header>
     </div>
   );
 }
