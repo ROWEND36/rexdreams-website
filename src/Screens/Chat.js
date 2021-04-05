2489e3af1e37e612d725f3798bea4547c48b4ceb
Rex-fortune <akpoyibofortune@gmail.com>
2021-04-05 10:32:01 -0700

    started invest

--- src/Screens/Chat.js
+++ src/Screens/Chat.js
@@ -5,7 +5,29 @@ import CardContent from "@material-ui/core/CardContent";
 import Box from "@material-ui/core/Box";
 
 function Chat(props) {
-  return <div></div>;
+  return (
+    <div>
+      <Container>
+        <Card className="shadow bg-secondary my-4">
+          <CardContent>
+            <Box my={4} mx={4}></Box>
+          </CardContent>
+        </Card>
+        <Card className="shadow bg-secondary my-4">
+          <CardContent>
+            <div className="d-flex flex-horizontal justify-content-between">
+              <div>
+                <Box my={4} mx={4}></Box>
+              </div>
+              <div>
+                <Box my={4} mx={4}></Box>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </Container>
+    </div>
+  );
 }
 
 export default Chat;
