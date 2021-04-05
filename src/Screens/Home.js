ab2e0c44879a677f734bb758f7d67c1735b8d66c
Rex-fortune <akpoyibofortune@gmail.com>
2021-04-05 10:28:31 -0700

    started work on the home page

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,7 +1,33 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Box from "@material-ui/core/Box";
 
 function Home(props) {
-  return <div></div>;
+  return (
+    <div>
+      <Container>
+        <Card className="shadow bg-secondary my-4">
+          <CardContent>
+            <Box my={4} mx={4}></Box>
+          </CardContent>
+        </Card>
+        <Card className="shadow bg-secondary my-4">
+          <CardContent>
+            <div className="d-flex flex-horizontal justify-content-between">
+              <div>
+                <Box my={4} mx={4}></Box>
+              </div>
+              <div>
+                <Box my={4} mx={4}></Box>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </Container>
+    </div>
+  );
 }
 
 export default Home;
