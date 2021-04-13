5395424c0ca4db3cf4be5558cf7dc09df8e5ca80
Rex-fortune <akpoyibofortune@gmail.com>
2021-04-13 14:40:07 -0700

    completed the welcome part of the home scree, yet to test it

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -11,10 +11,16 @@ import {
 } from "framer";
 import Bounce from "react-reveal/Bounce";
 import RubberBand from "react-reveal/RubberBand";
+import Spin from "react-reveal/Spin";
+import Wobble from "react-reveal/Wobble";
 import config from "react-reveal/globals";
 import Box from "@material-ui/core/Box";
 import "../App.css";
 import KingIcon from "../Images/KingIcon.png";
+import SettingsIcon from "@material-ui/icons/Settings";
+import AssistantPhotoIcon from "@material-ui/icons/AssistantPhoto";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import DeviceHubIcon from "@material-ui/icons/DeviceHub";
 
 const MobileViewWelcome1stHalf = () => {
   const x = useMotionValue(0);
@@ -79,7 +85,18 @@ const MobileViewWelcome2ndHalf = () => {
   );
 };
 
-const WelcomeHomeScreen = () => {
+const WelcomeHomeScreenMsg1 = () => {
+  config({ ssrFadeout: true });
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-200, 200], [1.5, 0.5]);
+  const [animate, cycle] = useCycle(
+    { scale: 0, rotate: 0 },
+    { scale: 1.5, rotate: 360 }
+  );
+  const [animate1, cycle1] = useCycle(
+    { scale: 0, rotate: 0 },
+    { scale: 1.5, rotate: 360, opacity: 1 }
+  );
   return (
     <Box m={4} className="child-center">
       <Bounce ssrFadeout>
@@ -136,7 +153,7 @@ const WelcomeHomeScreen = () => {
   );
 };
 
-function Home() {
+const WelcomeHomeScreenMsg2 = () => {
   config({ ssrFadeout: true });
   const x = useMotionValue(0);
   const scale = useTransform(x, [-200, 200], [1.5, 0.5]);
@@ -144,11 +161,104 @@ function Home() {
     { scale: 0, rotate: 0 },
     { scale: 1.5, rotate: 360 }
   );
-  const [animate1, cycle1] = useCycle(
-    { scale: 0, rotate: 0 },
-    { scale: 1.5, rotate: 360, opacity: 1 }
+  return (
+    <Box m={4} className="child-center">
+      <RubberBand ssrFadeout>
+        <motion.div
+          drag={"x"}
+          x={x}
+          scale={scale}
+          dragConstraints={{
+            left: -100,
+            right: 100,
+            top: -100,
+            bottom: 100,
+          }}
+          animate={animate}
+          transition={{ duration: 10 }}
+          onTap={() => cycle()}
+          className="Introduction-name d-none d-sm-inline-block"
+        >
+          Let's create <SettingsIcon /> your dreams together
+          <AssistantPhotoIcon />
+        </motion.div>
+        <MobileViewWelcome1stHalf />
+      </RubberBand>
+    </Box>
   );
+};
 
+const WelcomeHomeScreenMsg3 = () => {
+  config({ ssrFadeout: true });
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-200, 200], [1.5, 0.5]);
+  const [animate, cycle] = useCycle(
+    { scale: 0, rotate: 0 },
+    { scale: 1.5, rotate: 360 }
+  );
+  return (
+    <Box m={4} className="child-center">
+      <Spin ssrFadeout>
+        <motion.div
+          drag={"x"}
+          x={x}
+          scale={scale}
+          dragConstraints={{
+            left: -100,
+            right: 100,
+            top: -100,
+            bottom: 100,
+          }}
+          animate={animate}
+          transition={{ duration: 10 }}
+          onTap={() => cycle()}
+          className="Introduction-name d-none d-sm-inline-block"
+        >
+          We can bring your idea to life
+          <DeviceHubIcon />
+        </motion.div>
+        <MobileViewWelcome1stHalf />
+      </Spin>
+    </Box>
+  );
+};
+
+const WelcomeHomeScreenMsg4 = () => {
+  config({ ssrFadeout: true });
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-200, 200], [1.5, 0.5]);
+  const [animate, cycle] = useCycle(
+    { scale: 0, rotate: 0 },
+    { scale: 1.5, rotate: 360 }
+  );
+  return (
+    <Box m={4} className="child-center">
+      <Wobble ssrFadeout>
+        <motion.div
+          drag={"x"}
+          x={x}
+          scale={scale}
+          dragConstraints={{
+            left: -100,
+            right: 100,
+            top: -100,
+            bottom: 100,
+          }}
+          animate={animate}
+          transition={{ duration: 10 }}
+          onTap={() => cycle()}
+          className="Introduction-name d-none d-sm-inline-block"
+        >
+          Scroll down
+          <ArrowDownwardIcon /> to experience how
+        </motion.div>
+        <MobileViewWelcome1stHalf />
+      </Wobble>
+    </Box>
+  );
+};
+
+function Home() {
   return (
     <Scroll height={"100vh"} width={"100%"}>
       <div onScroll="" class="box">
